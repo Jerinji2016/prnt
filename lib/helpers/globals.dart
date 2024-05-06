53378d68f8c388fc44b06ef9e8a849015d446d79
Jerin <jerinji2016@gmail.com>
2024-05-07 01:25:30 +0530

    ip changed

--- lib/helpers/globals.dart
+++ lib/helpers/globals.dart
@@ -5,7 +5,7 @@ late final SharedPreferences sharedPreferences;
 class RedisConfig {
   RedisConfig._();
 
-  static const host = "54.196.177.19";
+  static const host = "54.91.111.189";
   static const port = 9736;
   static const password = "U.+uN_4G=GLeP=j7";
 }
